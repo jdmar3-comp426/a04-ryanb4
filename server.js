3035076cef832eb8965561e86ab85d710d269cb6
“ryanb4” <ryan4@live.unc.edu>
2021-11-04 17:32:12 -0400

    remove start message

--- server.js
+++ server.js
@@ -15,9 +15,10 @@ app.use(express.json());
 var HTTP_PORT = 5000;
 
 // Start server
-app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
-});
+// app.listen(HTTP_PORT, () => {
+//     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+// });
+
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app", (req, res, next) => {
     res.json({"message":"Your API works! (200)"});

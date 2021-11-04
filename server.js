7426e800feece22f631e33d6ae8b76f76349c47a
“ryanb4” <ryan4@live.unc.edu>
2021-11-04 16:33:45 -0400

    first commit

--- server.js
+++ server.js
@@ -2,22 +2,25 @@
 var express = require("express")
 var app = express()
 // Require database SCRIPT file
+var db = require('./database.js')
 
 // Require md5 MODULE
+var md5 = require("md5")
 
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
+var HTTP_PORT = 5000
 
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
-app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
+app.get("/", (req, res, next) => {
+    res.json({"message":"Your API is working! (200)"});
 	res.status(200);
 });
 
